aea011aef87437124e6c5c77ac807f5f0e8c5082
joshgold <joshgold@users.noreply.github.com>
2026-01-10 16:35:11 -0800

    Remove left margin from spending by category chart to fix excess padding

--- app/components/DashboardV2.tsx
+++ app/components/DashboardV2.tsx
@@ -503,7 +503,7 @@ export default function DashboardV2() {
             {categoryData.length > 0 ? (
               <div className="flex-1 min-h-[400px]">
                 <ResponsiveContainer width="100%" height="100%">
-                  <BarChart data={categoryData} margin={{ left: 10, right: 10, top: 5, bottom: 60 }}>
+                  <BarChart data={categoryData} margin={{ left: 0, right: 10, top: 5, bottom: 60 }}>
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis
                     dataKey="category"
